0aefa31775351212ce57c83759b4f8bd546de911
Jeffrey Fan <v-jifan@microsoft.com>
2016-05-13 19:25:25 +0800

    remove Fackbook, Google, Twitter, OAuth, MicrosoftAccount, AspNet Identity. keep only OpenIdConnect to v2.0 endpoint

--- webgallery/Controllers/ManageController.cs
+++ webgallery/Controllers/ManageController.cs
@@ -1,86 +1,31 @@
 ﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using WebGallery.Models;
 using System.Collections.Generic;
-using WebGallery.ViewModels;
-using System.Text.RegularExpressions;
 using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
 using WebGallery.Extensions;
+using WebGallery.Models;
+using WebGallery.ViewModels;
 
 namespace WebGallery.Controllers
 {
     [Authorize]
     public class ManageController : Controller
     {
-        private ApplicationSignInManager _signInManager;
-        private ApplicationUserManager _userManager;
-
         public ManageController()
         {
         }
 
-        public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
-        {
-            UserManager = userManager;
-            SignInManager = signInManager;
-        }
-
-        public ApplicationSignInManager SignInManager
-        {
-            get
-            {
-                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            }
-            private set 
-            { 
-                _signInManager = value; 
-            }
-        }
-
-        public ApplicationUserManager UserManager
-        {
-            get
-            {
-                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            }
-            private set
-            {
-                _userManager = value;
-            }
-        }
-
         //
         // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
+        public ActionResult Index()
         {
-            /*   ViewBag.StatusMessage =
-                   message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-                   : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-                   : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-                   : message == ManageMessageId.Error ? "An error has occurred."
-                   : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
-                   : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
-                   : "";
+            ViewBag.Name = ClaimsPrincipal.Current.FindFirst("name").Value;
+            ViewBag.PreferredUsername = ClaimsPrincipal.Current.FindFirst("preferred_username").Value;
+            ViewBag.EmailAddress = ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
 
-               var userId = User.Identity.GetUserId();
-               var model = new IndexViewModel
-               {
-                   HasPassword = HasPassword(),
-                   PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-                   TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-                   Logins = await UserManager.GetLoginsAsync(userId),
-                   BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
-               };
-               return View(model);*/
-
-            ViewBag.Username = User.Identity.GetUserName();
-            ViewBag.UserID = User.Identity.GetUserId();
             return View();
         }
 
@@ -458,145 +403,6 @@ namespace WebGallery.Controllers
                 return View("AppSubmit", submissions);
             }
 
-        }
-
-        //
-        // POST: /Manage/RemoveLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
-        {
-            ManageMessageId? message;
-            var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
-            if (result.Succeeded)
-            {
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-                if (user != null)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                }
-                message = ManageMessageId.RemoveLoginSuccess;
-            }
-            else
-            {
-                message = ManageMessageId.Error;
-            }
-          
-
-            return RedirectToAction("ManageLogins", new { Message = "Welcome" });
-        }
-
-      
-       
-        // GET: /Manage/ManageLogins
-        public ActionResult ManageLogins(ManageMessageId? message)
-        {
-            /*  ViewBag.StatusMessage =
-                  message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                  : message == ManageMessageId.Error ? "An error has occurred."
-                  : "";
-              var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-              if (user == null)
-              {
-                  return View("Error");
-              }
-              var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
-              var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
-              ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
-              return View(new ManageLoginsViewModel
-              {
-                  CurrentLogins = userLogins,
-                  OtherLogins = otherLogins
-              });*/
-
-            return View();
-        }
-
-        ////
-        //// POST: /Manage/LinkLogin
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult LinkLogin(string provider)
-        //{
-        //    // Request a redirect to the external login provider to link a login for the current user
-        //    return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
-        //}
-
-        //
-        // GET: /Manage/LinkLoginCallback
-        public async Task<ActionResult> LinkLoginCallback()
-        {
-            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
-            if (loginInfo == null)
-            {
-                return RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
-            }
-            var result = await UserManager.AddLoginAsync(User.Identity.GetUserId(), loginInfo.Login);
-            return result.Succeeded ? RedirectToAction("ManageLogins") : RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && _userManager != null)
-            {
-                _userManager.Dispose();
-                _userManager = null;
-            }
-
-            base.Dispose(disposing);
-        }
-
-#region Helpers
-        // Used for XSRF protection when adding external logins
-        private const string XsrfKey = "XsrfId";
-
-        private IAuthenticationManager AuthenticationManager
-        {
-            get
-            {
-                return HttpContext.GetOwinContext().Authentication;
-            }
-        }
-
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
-        }
-
-        private bool HasPassword()
-        {
-            var user = UserManager.FindById(User.Identity.GetUserId());
-            if (user != null)
-            {
-                return user.PasswordHash != null;
-            }
-            return false;
-        }
-
-        private bool HasPhoneNumber()
-        {
-            var user = UserManager.FindById(User.Identity.GetUserId());
-            if (user != null)
-            {
-                return user.PhoneNumber != null;
-            }
-            return false;
-        }
-
-        public enum ManageMessageId
-        {
-            AddPhoneSuccess,
-            ChangePasswordSuccess,
-            SetTwoFactorSuccess,
-            SetPasswordSuccess,
-            RemoveLoginSuccess,
-            RemovePhoneSuccess,
-            Error
-        }
-
-#endregion
+        }        
     }
 }
