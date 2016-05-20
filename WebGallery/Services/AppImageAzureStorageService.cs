5b605aee36933352f4ac8f556d70b6bfd2468599
Jeffrey Fan <v-jifan@microsoft.com>
2016-05-20 20:22:13 +0800

    shorten a property name for AppImageAzureStorageService

--- WebGallery/Services/AppImageAzureStorageService.cs
+++ WebGallery/Services/AppImageAzureStorageService.cs
@@ -7,21 +7,21 @@ namespace WebGallery.Services
     public class AppImageAzureStorageService : IAppImageStorageService
     {
         public string ConnectionString { get; } = ConfigurationManager.AppSettings["AzureStorageConnectionString"];
-        public string ContainerNameLogosScreenshots { get; } = CONTAINER_NAME_LOGOS_SCREENSHOTS;
+        public string ContainerName { get; } = CONTAINER_NAME_LOGOS_SCREENSHOTS;
 
         public void CreateContainerIfNotExists()
         {
-            AzureStorageHelper.CreateContainerIfNotExists(ConnectionString, ContainerNameLogosScreenshots);
+            AzureStorageHelper.CreateContainerIfNotExists(ConnectionString, ContainerName);
         }
 
         public string Upload(int submissionId, string imageName, Stream stream)
         {
-            return AzureStorageHelper.Upload(ConnectionString, ContainerNameLogosScreenshots, GetBlobName(submissionId, imageName), stream);
+            return AzureStorageHelper.Upload(ConnectionString, ContainerName, GetBlobName(submissionId, imageName), stream);
         }
 
         public void Delete(int submissionId, string imageName)
         {
-            AzureStorageHelper.Delete(ConnectionString, ContainerNameLogosScreenshots, GetBlobName(submissionId, imageName));
+            AzureStorageHelper.Delete(ConnectionString, ContainerName, GetBlobName(submissionId, imageName));
         }
 
         private static string GetBlobName(int submissionId, string imageName)
