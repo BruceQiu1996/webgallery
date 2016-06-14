36b9aed72723875a73f0eb8d0fdb339dcefb0f95
Sharpeli <sharpel@hotmail.com>
2016-06-14 15:07:19 +0800

    add back logic view layout and css of app detail

--- WebGallery/ViewModels/AppDetailViewModel.cs
+++ WebGallery/ViewModels/AppDetailViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace WebGallery.ViewModels
+﻿using System.Collections.Generic;
+using WebGallery.Models;
+
+namespace WebGallery.ViewModels
 {
     public class AppDetailViewModel
     {
+        public Submission Submission { get; set; }
+        public SubmissionLocalizedMetaData MetaData { get; set; }
+        public IList<ProductOrAppCategory> Categories { get; set; }
+        public IList<string> ScreenshotUrls
+        {
+            get
+            {
+                return new List<string>
+                {
+                    Submission.ScreenshotUrl1,
+                    Submission.ScreenshotUrl2,
+                    Submission.ScreenshotUrl3,
+                    Submission.ScreenshotUrl4,
+                    Submission.ScreenshotUrl5,
+                    Submission.ScreenshotUrl6
+                };
+            }
+        }
     }
 }
