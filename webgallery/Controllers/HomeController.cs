74e9d5f0909ffcbcb0f4baafe01b55d3d7a26a60
Jeffrey Fan <v-jifan@microsoft.com>
2016-06-27 15:02:03 +0800

    remove home.about that is added by scaffolding

--- webgallery/Controllers/HomeController.cs
+++ webgallery/Controllers/HomeController.cs
@@ -17,13 +17,6 @@ namespace WebGallery.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             return View(new ContactModel());
