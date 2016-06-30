01ab407386518a8133c0656e6424a2d43e5f7209
Jeffrey Fan <v-jifan@microsoft.com>
2016-06-30 15:57:00 +0800

    add profiles/{submitterId}

---
+++ WebGallery/ViewModels/AccountDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebGallery.Models;
+
+namespace WebGallery.ViewModels
+{
+    public class AccountDetailViewModel
+    {
+        public Submitter Submittership { get; set; }
+        public SubmittersContactDetail ContactInfo { get; set; }
+    }
+}
