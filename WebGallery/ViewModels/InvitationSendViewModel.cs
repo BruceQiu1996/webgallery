3edfd1d066a16e52dc73314f0fb7af87b4cd8559
Sharpeli <sharpel@hotmail.com>
2016-07-21 20:15:24 +0800

    complete the globalization of app owner invitation

--- WebGallery/ViewModels/InvitationSendViewModel.cs
+++ WebGallery/ViewModels/InvitationSendViewModel.cs
@@ -7,21 +7,22 @@ namespace WebGallery.ViewModels
     {
         public Submission Submission { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "FirstNameRequired", ErrorMessageResourceType = typeof(Resources.Invitation))]
+        [Display(Name = "FirstNameLabel", ResourceType = typeof(Resources.Invitation))]
         public string FirstName { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "LastNameRequired", ErrorMessageResourceType = typeof(Resources.Invitation))]
+        [Display(Name = "LastNameLabel", ResourceType = typeof(Resources.Invitation))]
         public string LastName { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [Display(Name = "Email Address")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "EMailRequired", ErrorMessageResourceType = typeof(Resources.Invitation))]
+        [Display(Name = "eMailLabel", ResourceType = typeof(Resources.Invitation))]
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
-        [Compare("EmailAddress")]
-        [Display(Name = "Confirm Email Address")]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceName = "ConfirmationEMailRequired", ErrorMessageResourceType = typeof(Resources.Invitation))]
+        [Compare("EmailAddress", ErrorMessageResourceName = "EMailAddressesDoNotMatch", ErrorMessageResourceType = typeof(Resources.Invitation))]
+        [Display(Name = "ConfirmEmailLabel", ResourceType = typeof(Resources.Invitation))]
         public string ConfirmEmailAddress { get; set; }
     }
 }
