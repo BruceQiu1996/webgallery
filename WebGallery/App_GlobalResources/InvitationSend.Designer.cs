b5c9a20cb79f92fda32a95281d0719eb6a0b9f71
Sharpeli <sharpel@hotmail.com>
2016-07-22 13:43:14 +0800

    complete the globalization of invitation send

--- WebGallery/App_GlobalResources/InvitationSend.Designer.cs
+++ WebGallery/App_GlobalResources/InvitationSend.Designer.cs
@@ -22,14 +22,14 @@ namespace Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Invitation {
+    public class InvitationSend {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Invitation() {
+        internal InvitationSend() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebGallery.App_GlobalResources.Invitation", typeof(Invitation).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WebGallery.App_GlobalResources.InvitationSend", typeof(InvitationSend).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
