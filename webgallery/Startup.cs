6ad29637623258b2ba9d5bca8e2dbd9daea285e0
Sharpeli <sharpel@hotmail.com>
2016-08-05 17:36:21 +0800

    improve globalization

--- webgallery/Startup.cs
+++ webgallery/Startup.cs
@@ -22,7 +22,7 @@ namespace WebGallery
         public IController Create(RequestContext requestContext, Type controllerType)
         {
             var cookie = requestContext.HttpContext.Request.Cookies["LanguagePreference"];
-            if (cookie != null)
+            if (cookie != null && !string.IsNullOrWhiteSpace(cookie.Value))
             {
                 try
                 {
@@ -34,17 +34,28 @@ namespace WebGallery
                 // If the cookie.Value can't be used to get a CultureInfo, we keep the CurrentCulture unchanged
                 catch { }
             }
+            else
+            {
+                var languages = requestContext.HttpContext.Request.UserLanguages;
 
-            // the old site use the old language code "zh-chs", "zh-cht" as resource file suffix, and current code of them are "zh-CN" and "zh-TW"
-            if (Thread.CurrentThread.CurrentCulture.Name.ToLower() == "zh-cn")
-            {
-                Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-chs");
+                // the old site use the old language code "zh-chs", "zh-cht" as resource file suffix, and current code of them are "zh-CN" and "zh-TW"
+                if (languages != null && languages.Length > 0)
+                {
+                    switch (languages[0].ToLower())
+                    {
+                        case "zh-cn": Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-chs"); break;
+                        case "zh-tw": Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-cht"); break;
+                        default:
+                            try
+                            {
+                                Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(languages[0]);
+                            }
+                            catch { }
+                            break;
+                    }
+                }
+                Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
             }
-            if (Thread.CurrentThread.CurrentCulture.Name.ToLower() == "zh-tw")
-            {
-                Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-cht");
-            }
-            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
 
             return DependencyResolver.Current.GetService(controllerType) as IController;
         }
