fe29f362ef858d3d193aa02ac1c7066bf0d88e6c
Jeffrey Fan <v-jifan@microsoft.com>
2018-02-26 02:20:22 +0000

    Merged PR 264: this fixes bug #313 - cookie law violation
    this fixes bug #313
    
    Related work items: #313

--- WebAppGallery/App_Start/BundleConfig.cs
+++ WebAppGallery/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace WebGallery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerycookie").Include(
+                        "~/Scripts/jquery.cookie-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate.*",
                         "~/Scripts/jquery.validate.unobtrusive.*"));
